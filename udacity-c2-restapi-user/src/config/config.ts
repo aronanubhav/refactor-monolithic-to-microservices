ab99d03ab32af40d64fdfaf3fb7a4150fcf06297
aronanubhav <68130122+aronanubhav@users.noreply.github.com>
2020-09-21 10:17:44 +0100

    Update config.ts

--- udacity-c2-restapi-user/src/config/config.ts
+++ udacity-c2-restapi-user/src/config/config.ts
@@ -1,14 +1,14 @@
 export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password': process.env.POSTGRES_PASSWORD,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
+  'username': 'aronanubhav',
+  'password': 'Covid2020',
+  'database': 'postgres',
+  'host': 'rds-aronanubhav.cjzgfj3nh7rd.eu-west-1.rds.amazonaws.com',
   'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
+  'aws_region': 'eu-west-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 's3-aronanubhav-dev',
+  'url': 'http://localhost:8100',
   'jwt': {
-    'secret': process.env.JWT_SECRET,
+    'secret': 'hello',
   },
 };
