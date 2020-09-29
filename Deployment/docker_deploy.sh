e7a9fc71a1ea6eaf37e2efb4816db7fbe5d7951d
aronanubhav <68130122+aronanubhav@users.noreply.github.com>
2020-09-29 21:49:07 +0100

    Update docker_deploy.sh

--- Deployment/docker_deploy.sh
+++ Deployment/docker_deploy.sh
@@ -3,4 +3,4 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker push aronanubhav/udacity-restapi-user
 docker push aronanubhav/udacity-restapi-feed
 docker push aronanubhav/reverseproxy
-docker push aronanubhav/udacity-frontend:local
+docker push aronanubhav/udacity-frontend
